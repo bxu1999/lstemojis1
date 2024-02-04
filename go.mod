0e85345cc2373bcd55141741057d8a7ac8576fff
bxu account <bxu@rojapevm1.fyre.ibm.com>
2024-02-04 15:54:43 -0800

    First commit for this project lstemojis1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module lstemojis1
+
+go 1.20
+
+require github.com/hackebrot/turtle v0.2.0
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+  "github.com/hackebrot/turtle"
+)
+
+func main() {
+
+  emoji, ok := turtle.Emojis["smiley"]
+  
+  if !ok {
+    fmt.Println("No emoji found.")
+  } else {
+    fmt.Println(emoji.Char)
+  }
+}
